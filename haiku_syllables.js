f5043231d3343ddc0f96646e41e2a9364a673908
cloworm <chloe.echikson@gmail.com>
2016-07-19 14:05:25 -0400

    Assign outlier words to 0 syllables

--- haiku_syllables.js
+++ haiku_syllables.js
@@ -1,9 +1,6 @@
 var fs = require("fs");
 var cmudictFile = readCmudictFile('./cmudict.txt');
 
-// for each line, count how many numbers, n are in it
-// put first word into an array with index n
-
 function readCmudictFile(file) {
   return fs.readFileSync(file).toString();
 }
@@ -23,6 +20,8 @@ function countSyllables(pronunciation) {
   var syllables = pronunciation.match(/\d/g);
   if (syllables) {
     return syllables.length;
+  } else {
+    return 0;
   }
 }
 
