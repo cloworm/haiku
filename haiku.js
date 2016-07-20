4746bc06d5872d00d7271617628bf4b53acf9508
cloworm <chloe.echikson@gmail.com>
2016-07-20 14:16:21 -0400

    Removed comments

--- haiku.js
+++ haiku.js
@@ -46,65 +46,21 @@ function randomBookLine(book) {
 
 function countBookLineSyllables(line) {
   var syllables = 0;
+  var syllablesCounted = 0;
   var wordBank = haikuSyllables.arrayBySyllables(cmudictFile);
   line.forEach(function(word) {
     for(var key in wordBank) {
       if (wordBank[key].includes(word.toUpperCase())) {
         syllables += parseInt(key);
+        syllablesCounted++;
       }
     }
   })
+  if (syllablesCounted !== line.length) {
+    syllables = 0;
+  }
   return syllables;
 }
-// function createHaikuFromBook(structure, book) {
-//   // split book up into array by sentences
-//   // add up syllable count of sentences
-//   // add each sentence to an object
-//   // return random line matching necessary syllables
-//   var bookByLine = book.replace(/\r\n/g, " ").split(/\.|\?|\!/g);
-//   var haikuLine = [];
-//   var haiku = structure.map(function(syllables) {
-//     // while (haikuLine.length < 1) {
-//       var randomSentenceIndex = Math.floor(Math.random() * bookByLine.length);
-//       var randomSentence = bookByLine[randomSentenceIndex].split(/\s/);
-//       randomSentence.shift();
-//       var randomSentenceSyllables = convertSentenceToSyllables(randomSentence);
-//       var haikuLine = returnHaikuLine(randomSentence, randomSentenceSyllables, syllables);
-//       console.log(haikuLine);
-//     // }
-//     return haikuLine;
-//   })
-//   return haiku;
-// }
-
-// function convertSentenceToSyllables(randomSentence) {
-//   var wordBank = haikuSyllables.arrayBySyllables(cmudictFile);
-//   var syllableArray = [];
-//   randomSentence.forEach(function(word) {
-//     for (var key in wordBank) {
-//       if (wordBank[key].includes(word.toUpperCase())) {
-//         syllableArray.push(key);
-//       }
-//     }
-//   });
-//   return syllableArray;
-// }
-
-// function returnHaikuLine(randomSentence, randomSentenceSyllables, syllables) {
-//   var haikuLine = [];
-//   var syllableCount = 0;
-//   for (var i = 0; i < randomSentence.length; i++) {
-//     if (syllableCount < syllables) {
-//       haikuLine.push(randomSentence[i]);
-//       syllableCount += randomSentenceSyllables[i];
-//     }
-//   }
-//   if (syllableCount !== syllables) {
-//     haikuLine = [];
-//   }
-//   console.log(haikuLine);
-//   return haikuLine;
-// }
 
 module.exports = {
   createHaiku: createHaiku,
